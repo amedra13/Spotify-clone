d45a89495ed49d00dafec13c031b03066e1b13a0
Andres Medrano <amedra13@gmail.com>
2020-09-19 15:34:51 -0700

    setting up data layer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Player from './Player';
+import Login from './Login';
 import './App.css';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import { useDataLayerValue } from './DataLayer';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-	return <div className="app">Spotify clone</div>;
+	const [token, setToken] = useState(null);
+	const [{ user }, dispatch] = useDataLayerValue();
+
+	useEffect(() => {
+		const hash = getTokenFromUrl();
+		window.location.hash = '';
+		const _token = hash.access_token;
+
+		if (_token) {
+			setToken(_token);
+			spotify.setAccessToken(_token);
+			spotify.getMe().then((user) => {
+				dispatch({ type: 'SET_USER', user: user });
+			});
+		}
+
+		console.log('I have a token >>', token);
+	}, [token, dispatch]);
+
+	console.log(':)', user);
+
+	return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
