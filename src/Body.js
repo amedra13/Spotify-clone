44db709ad26a3b4157f313f03fd385ad80ebeaae
Andres Medrano <amedra13@gmail.com>
2020-09-21 23:31:56 -0700

    finished header

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header';
 import './body.css';
 
-const Body = () => {
+const Body = ({ spotify }) => {
 	return (
 		<div className="body">
 			<h1>Body Component</h1>
+			<Header spotify={spotify} />
 		</div>
 	);
 };
--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,8 @@ export const initialState = {
 	playlists: [],
 	playing: false,
 	item: null,
-	token:
-		'BQDUmiAnNGN1AL3EI7nlWT6Jto-nSUYjxiiB2dSzrElmsKs3UvusIB1KHdu62qGgcnHC-3KwmIG--oLjYm_Ce3RWHjeIoUq9yxBnvzfFjQ8eQWEHS6QRA5s9NuYZu9XuRbkBEmGjCjH-DhJlW_ZPM_pm-Q',
+	// token:
+	// 	'BQDUmiAnNGN1AL3EI7nlWT6Jto-nSUYjxiiB2dSzrElmsKs3UvusIB1KHdu62qGgcnHC-3KwmIG--oLjYm_Ce3RWHjeIoUq9yxBnvzfFjQ8eQWEHS6QRA5s9NuYZu9XuRbkBEmGjCjH-DhJlW_ZPM_pm-Q',
 };
 
 const reducer = (state, action) => {
